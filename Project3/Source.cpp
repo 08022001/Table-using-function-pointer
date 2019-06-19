c224b6b425f6934a7ece5a7066f5c319367cb844
Pallavi <pallavishingade59@gmail.com>
2019-06-19 21:07:53 +0530

    added comments

--- Project3/Source.cpp
+++ Project3/Source.cpp
@@ -3,6 +3,8 @@
 #include<vector>
 
 using namespace std;
+
+//this function takes an int and print table on console
 void Table(int x)
 {
 	for (int i = 1; i <= 10; i++)
@@ -10,9 +12,10 @@ void Table(int x)
 		cout <<i * x << endl;
 	}
 }
-void TableNo(vector<int> Value , void(*Calculate)(int))
+//for each value in the vector a function is called
+void TableNo(vector<int> Value , void(*Calculate)(int)) //void(*NewFunction)(params))
 {
-	for (int x : Value)
+	for (int x : Value) //x is element of value
 		Calculate(x);
 }
 int main()
